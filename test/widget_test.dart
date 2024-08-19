b88d8f526c19c547ad5be768e8a0c152d4213ea3
zhouwj <zhouwjfi@163.com>
2024-08-19 17:14:46 +0800

    trigger action

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-
+//test
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {});
 }
